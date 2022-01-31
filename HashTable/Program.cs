aaa5ca7505ee9aff0735716b72280140cf5538a9
Aniketw1551 <aniketwaghmare976@gmail.com>
2022-01-31 20:19:59 +0530

    [Aniket] Adding Frequency of words in large paragraph program

--- HashTable/Program.cs
+++ HashTable/Program.cs
@@ -11,7 +11,7 @@ namespace HashTable
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to HashTable Program");
-            string paragraph = "To be or not to be";
+            string paragraph = "Nory was a Catholic because her mother was a Catholic, and Nory’s mother was a Catholic because her father was a Catholic";
             CountFrequencyofWords(paragraph);
             Console.ReadLine();
         }
